c46c450aace39e845676471ed2aea5ddef112cef
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-03-10 11:48:06 +0100

    Ejercicio 4 Finalizado

---
+++ JavaModC2MetodosArrays/src/Ejercicio4.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author aitor
+ *
+ */
+public class Ejercicio4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("EJERCICIO 4 - FACTORIAL");
+		System.out.println("Introduce un numero por teclado: ");
+		double numero = sc.nextDouble();
+		sc.close();
+		double resultado = calcularFactorial(numero);
+		System.out.println("El Factorial de "+numero+ " da como resultado: "+resultado);
+
+	}
+	
+	public static double calcularFactorial(double numero) {
+		for(double i = numero-1; i>=1; i--) {
+			numero = numero * i;
+		}
+		return numero;
+	}
+
+}
